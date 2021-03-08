5c747613c14036ed61241c92a0f84bc214cd682e
alexkaz357 <alexkaz357@gmail.com>
2021-03-08 12:53:51 +0200

    'Deploy'

---
+++ precache-manifest.64ae726b4a9db5822dc89cc2a4e1b43d.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4873445807e0f2f099c65e6a7592930",
+    "url": "/myteam/index.html"
+  },
+  {
+    "revision": "6a268869279217decc10",
+    "url": "/myteam/static/css/main.2e00d8ef.chunk.css"
+  },
+  {
+    "revision": "53f45c50da057d182ede",
+    "url": "/myteam/static/js/2.57cf324f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/myteam/static/js/2.57cf324f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a268869279217decc10",
+    "url": "/myteam/static/js/main.28a9a0ca.chunk.js"
+  },
+  {
+    "revision": "e395bcfdadfe4c23c27c",
+    "url": "/myteam/static/js/runtime-main.34fc45ec.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/myteam/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "342d3ecdd8151ebb80e30d61d2575541",
+    "url": "/myteam/static/media/about-mobile-bottom.342d3ecd.svg"
+  },
+  {
+    "revision": "5902882649d01af10e24454aa34d0723",
+    "url": "/myteam/static/media/about-mobile-top.59028826.svg"
+  },
+  {
+    "revision": "149ad4d8c30805abb3e44d66dc9b96dc",
+    "url": "/myteam/static/media/about-tablet.149ad4d8.svg"
+  },
+  {
+    "revision": "6dfe30c39faa9e3983cc8e417aa50338",
+    "url": "/myteam/static/media/avatar-aden.6dfe30c3.jpg"
+  },
+  {
+    "revision": "d1d950e09ee94a2bbdd4627e65143cbc",
+    "url": "/myteam/static/media/avatar-aiysha.d1d950e0.jpg"
+  },
+  {
+    "revision": "60ff8909e4d598e25c144f0b47d988e3",
+    "url": "/myteam/static/media/avatar-arthur.60ff8909.jpg"
+  },
+  {
+    "revision": "4d038c47320d35305c5dd573d5855ff3",
+    "url": "/myteam/static/media/avatar-cruz.4d038c47.jpg"
+  },
+  {
+    "revision": "7e1826dab65af3e42a2f9a989131de06",
+    "url": "/myteam/static/media/avatar-drake.7e1826da.jpg"
+  },
+  {
+    "revision": "ae9abcb57a9d23ecaad395a1e0175a21",
+    "url": "/myteam/static/media/avatar-griffin.ae9abcb5.jpg"
+  },
+  {
+    "revision": "f16826cacf2b1a460d2eab969d343d0a",
+    "url": "/myteam/static/media/avatar-kady.f16826ca.jpg"
+  },
+  {
+    "revision": "0ee32a0938ed725cc3c97287c9dfea73",
+    "url": "/myteam/static/media/avatar-nikita.0ee32a09.jpg"
+  },
+  {
+    "revision": "00dc85d0f8c874f627cedb798221c9c5",
+    "url": "/myteam/static/media/bg-pattern-about-1-mobile-nav-1.00dc85d0.svg"
+  },
+  {
+    "revision": "3976b93d79662b2af71dbc5ceb3cf596",
+    "url": "/myteam/static/media/bg-pattern-about-2-contact-1.3976b93d.svg"
+  },
+  {
+    "revision": "fb377e1325db519c8924c95777ed2e16",
+    "url": "/myteam/static/media/bg-pattern-about-4.fb377e13.svg"
+  },
+  {
+    "revision": "c794ab3a8e4bba6aacaae8101210277e",
+    "url": "/myteam/static/media/bg-pattern-contact-2.c794ab3a.svg"
+  },
+  {
+    "revision": "e45a3d0d8de698bfbea8ddc00225fd8e",
+    "url": "/myteam/static/media/bg-pattern-home-1.e45a3d0d.svg"
+  },
+  {
+    "revision": "2fd804c4ec4e139d392e43c599bd277b",
+    "url": "/myteam/static/media/bg-pattern-home-2.2fd804c4.svg"
+  },
+  {
+    "revision": "8fced9299fa222aa1131f36e3d689453",
+    "url": "/myteam/static/media/bg-pattern-home-3.8fced929.svg"
+  },
+  {
+    "revision": "fe4f785a6475f2ea47465eb9a6fcda38",
+    "url": "/myteam/static/media/bg-pattern-home-4-about-3.fe4f785a.svg"
+  },
+  {
+    "revision": "458c9fcdfef1f53beea9819bd3a2b4e8",
+    "url": "/myteam/static/media/bg-pattern-home-5.458c9fcd.svg"
+  },
+  {
+    "revision": "1f80fc3fb7d03dd4d7a60927c76a30f2",
+    "url": "/myteam/static/media/bg-pattern-home-6-about-5.1f80fc3f.svg"
+  },
+  {
+    "revision": "7a1f08d1815495d2ffed1fa8b34ebb4a",
+    "url": "/myteam/static/media/get-started-mobile.7a1f08d1.svg"
+  },
+  {
+    "revision": "1ece178695707c353a82b30a19b11d2e",
+    "url": "/myteam/static/media/icon-chart.1ece1786.svg"
+  },
+  {
+    "revision": "6a06cfad75cd90283d3a0e10c16ae684",
+    "url": "/myteam/static/media/icon-close.6a06cfad.svg"
+  },
+  {
+    "revision": "6cb0f5760e3e8a0f4567755fb5250b32",
+    "url": "/myteam/static/media/icon-cog.6cb0f576.svg"
+  },
+  {
+    "revision": "29f30f053ed50fb71562abf0aba18884",
+    "url": "/myteam/static/media/icon-cross.29f30f05.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/myteam/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "df4410aead66f0adfa80154f254dc562",
+    "url": "/myteam/static/media/icon-hamburger.df4410ae.svg"
+  },
+  {
+    "revision": "d99f6328cbfa9c4ef459769d8e18357c",
+    "url": "/myteam/static/media/icon-linkedin.d99f6328.svg"
+  },
+  {
+    "revision": "05759150ed61077a65185636252594d7",
+    "url": "/myteam/static/media/icon-person.05759150.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/myteam/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "efcf9a22216f31e99d42af206a8227a9",
+    "url": "/myteam/static/media/icon-quotes.efcf9a22.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/myteam/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "22c2459ee9b2f81c568b15fdfa3e84bf",
+    "url": "/myteam/static/media/logo.22c2459e.svg"
+  }
+]);
